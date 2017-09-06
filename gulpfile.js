4d49f8fd67163969c74ce60a6e5dc1a5fa3a62e1
Mike Fraser <mikejfraser@me.com>
2017-09-06 12:55:54 +0100

    node packages downloaded, gulpfile started (not finished)

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+// =======================
+// Kirby Patterns Gulpfile
+// =======================
+
+// Dependencies
+// -------------
+
+// Config
+// -------------
+
+// JS
+// -------------
+gulp.task('js', function() {
+
+});
+
+// CSS
+// -------------
+gulp.task('css', function() {
+    
+});
+
+// IMG
+// -------------
+gulp.task('img', function() {
+    
+});
+
+// SVG
+// -------------
+gulp.task('svg', function() {
+    
+});
+
+// BROWSERSYNC
+// -------------
+gulp.task('sync', function() {
+    
+});
+
+// WATCH
+// -------------
+gulp.task('watch', function() {
+    
+});
+
+// DEFAULT
+// -------------
+gulp.task('default', [
+    'js', 
+    'css',
+    'img',
+    'svg',
+  ]);
